684c08eafb066637604bacdb48a6cf78c325a1c3
Adam Werbach <adam@planet10b.com>
2026-04-01 00:23:15 -0700

    fix: install @sendgrid/mail and correct landing.css import path in newsletter page

--- src/app/c/[slug]/newsletter/[date]/page.tsx
+++ src/app/c/[slug]/newsletter/[date]/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 
-import "../../../../../landing.css";
+import "../../../../landing.css";
 
 import { getNewsletterEdition } from "@/lib/newsletter";
 import { listPublicCitiesForSitemap } from "@/lib/publicApiClient";
